0886fb56a937f1d3823089ae781a162869a382d0
Eric Zhou <eric.m.zhou@gmail.com>
2022-05-14 22:36:40 -0700

    Finished maximized partitions algorithm

--- wordle.py
+++ wordle.py
@@ -2,6 +2,8 @@ import sys
 import random
 import re
 import copy
+import pickle
+import time
 from enum import Enum
 from colorama import init
 from termcolor import colored
@@ -9,9 +11,9 @@ from termcolor import colored
 '''
 commands:
 wordle - picks a random word and plays with you
-wordle solver - solver for wordle
+wordle solver words/letters - solver for wordle
 wordle test - does build run stats, picks a random string for file names
-wordle build strategy_file - builds strategy
+wordle build algorithm strategy_file - builds strategy
 wordle run strategy_file - creates leaderboard result file
 wordle stats result_file - prints relevant stats (avg guess, etc)
 wordle play strategy_file answer - print guesses leading to answer
@@ -39,10 +41,13 @@ ANSWER_LIST_FILE = "wordle-nyt-answers-alphabetical.txt"
 GUESS_LIST_FILE = "wordle-nyt-allowed-guesses.txt"
 MAX_GUESSES = 6
 
-class C(Enum):
-	GREEN = 1
-	YELLOW = 2
-	GREY = 3
+def load_answer_guess_lists():
+	with open(ANSWER_LIST_FILE) as answer_file:
+		with open(GUESS_LIST_FILE) as guess_file:
+			answer_list = answer_file.read().splitlines()
+			guess_list = guess_file.read().splitlines()
+			guess_list.extend(answer_list)
+			return (answer_list, guess_list)
 
 class Information:
 	def __init__(self, green=None, yellow=None, grey=None):
@@ -55,7 +60,7 @@ class Information:
 		return "wordle.Information({0}, {1}, {2})".format(self.green, self.yellow, self.grey)
 
 	def set_green(self, position, letter):
-		if self.green[position] is not None:
+		if self.green[position] is not None and self.green[position] != letter:
 			raise Exception("Letter already there")
 		self.green[position] = letter
 		self.validate()
@@ -71,9 +76,9 @@ class Information:
 	def plus(self, guess, hint):
 		info = copy.deepcopy(self)
 		for i in range(5):
-			if hint[i] == C.GREEN:
+			if hint[i] == 'G':
 				info.set_green(i, guess[i])
-			elif hint[i] == C.YELLOW:
+			elif hint[i] == 'Y':
 				info.add_yellow(i, guess[i])
 			else:
 				info.add_grey(guess[i])
@@ -117,15 +122,61 @@ def get_hint(guess, answer):
 	output = []
 	for i in range(5):
 		if guess[i] == answer[i]:
-			output.append(C.GREEN)
+			output.append("G")
 		elif guess[i] in answer:
-			output.append(C.YELLOW)
+			output.append("Y")
 		else:
-			output.append(C.GREY)
-	return output
+			output.append("-")
+	return "".join(output)
 
-def hint_from_string(hint_string):
-	return [{"G": C.GREEN, "Y": C.YELLOW, "-": C.GREY}[c] for c in hint_string]
+class Node:
+	def __init__(self, guess):
+		self.guess = guess
+		self.child = dict() # hint -> Node
+
+def build_maximize_partitions(answer_list, guess_list, level=0):
+	print("Level {0}, num of answers: {1}, some answers: {2}".format(level, len(answer_list), ", ".join(answer_list[:5])))
+	if level >= 6:
+		raise Exception("Search got too deep.")
+	if len(answer_list) == 1:
+		return Node(answer_list[0])
+	best_guess = None
+	best_partitions = dict()
+	for guess in guess_list:
+		partitions = {}
+		for answer in answer_list:
+			hint = get_hint(guess, answer)
+			if hint not in partitions:
+				partitions[hint] = [answer]
+			else:
+				partitions[hint].append(answer)
+		p, bp = len(partitions), len(best_partitions)
+		if p > bp or (p == bp and 'GGGGG' in partitions):
+			print("Best guess: {0}, # of paritions: {1}".format(guess, len(partitions)))
+			best_guess = guess
+			best_partitions = partitions
+	strategy = Node(best_guess)
+	for hint, answers in best_partitions.items():
+		print("Hint: {0}".format(hint))
+		strategy.child[hint] = build_maximize_partitions(answers, guess_list, level + 1)
+	return strategy
+
+def play(answer, strategy):
+	guesses = []
+	node = strategy
+	while True:
+		guesses.append(node.guess)
+		if node.guess == answer:
+			break
+		hint = get_hint(node.guess, answer)
+		node = node.child[hint]
+	return guesses
+
+###############################
+#
+# Human Interaction Section
+#
+###############################
 
 def solver_by_words():
 	info = Information()
@@ -135,11 +186,10 @@ def solver_by_words():
 		if not re.fullmatch('[a-z][a-z][a-z][a-z][a-z]', guess):
 			print("Bad format; guess 5 lower case letters.")
 			continue
-		hint_string = input("Hints: ")
-		if not re.fullmatch("[-GY][-GY][-GY][-GY][-GY]", hint_string):
+		hint = input("Hints: ")
+		if not re.fullmatch("[-GY][-GY][-GY][-GY][-GY]", hint):
 			print("Bad format. Use 'G' for green letters, 'Y' for yellow letters, '-' for grey letters.")
 			continue
-		hint = hint_from_string(hint_string)
 		info = info.plus(guess, hint)
 		print("Possible answers: ")
 		new_list = []
@@ -196,10 +246,8 @@ def play_human():
 				break
 
 		hint = get_hint(guess, answer)
-		hint_display = ""
-		for i in range(5):
-			color = {C.GREEN: 'on_green', C.YELLOW: 'on_yellow', C.GREY: 'on_grey'}[hint[i]]
-			hint_display += colored(guess[i], 'white', color)
+		colors = [{'G': 'on_green', 'Y': 'on_yellow', '-': 'on_grey'}[h] for h in hint]
+		hint_display = "".join([colored(guess[i], 'white', colors[i]) for i in range(5)])
 		print("          result: " + hint_display)
 
 		if guess == answer:
@@ -208,13 +256,9 @@ def play_human():
 	else:
 		print("--- Too Bad! The correct word was {0}. ---".format(answer))
 
-def load_answer_guess_lists():
-	with open(ANSWER_LIST_FILE) as answer_file:
-		with open(GUESS_LIST_FILE) as guess_file:
-			answer_list = answer_file.read().splitlines()
-			guess_list = guess_file.read().splitlines()
-			guess_list.extend(answer_list)
-			return (answer_list, guess_list)
+ALGORITHMS = {
+	'max_part': build_maximize_partitions
+}
 
 def main():
 	args = sys.argv[1:]
@@ -226,20 +270,48 @@ def main():
 		elif args[1] == "letters":
 			solver_by_letters()
 	elif args[0] == "test":
-		pass
+		algorithm = ALGORITHMS[args[1]]
+		file_name = str(int(time.time()))
+		with open('strategies/' + file_name, 'wb') as sf:
+			with open('results/' + file_name + '.txt', 'wt') as rf:
+				answer_list, guess_list = load_answer_guess_lists()
+				strategy = algorithm(answer_list, guess_list)
+				pickle.dump(strategy, sf, pickle.HIGHEST_PROTOCOL)
+				for answer in answer_list:
+					guesses = play(answer, strategy)
+					rf.write(", ".join(guesses) + '\n')
 	elif args[0] == "build":
-		strategy_file = args[1]
-		pass
+		algorithm = ALGORITHMS[args[1]]
+		strategy_file = args[2]
+		with open(strategy_file, 'wb') as sf:
+			answer_list, guess_list = load_answer_guess_lists()
+			strategy = algorithm(answer_list, guess_list)
+			pickle.dump(strategy, sf, pickle.HIGHEST_PROTOCOL)
 	elif args[0] == "run":
 		strategy_file = args[1]
-		pass
+		results_file = args[2]
+		with open(strategy_file, 'rb') as sf:
+			with open(results_file, 'wt') as rf:
+				strategy = pickle.load(sf)
+				answer_list, _ = load_answer_guess_lists()
+				for answer in answer_list:
+					guesses = play(answer, strategy)
+					rf.write(", ".join(guesses) + '\n')
 	elif args[0] == "stats":
 		results_file = args[1]
-		pass
+		with open(results_file, 'rt') as rf:
+			guess_lines = rf.read().splitlines()
+			total = 0
+			for line in guess_lines:
+				total += len(line.split(", "))
+			print("Avg guesses: {0}, Total guesses: {1}".format(total / len(guess_lines), total))
 	elif args[0] == "play":
-		strategy_file = args[2]
-		answer = args[1]
-		pass
+		strategy_file = args[1]
+		answer = args[2]
+		with open(strategy_file, 'rb') as sf:
+			strategy = pickle.load(sf)
+			guesses = play(answer, strategy)
+			print(", ".join(guesses))
 	else:
 		print("Invalid arguments")
 
